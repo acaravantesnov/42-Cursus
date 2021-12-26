0db31acf6e6fdf6835fe8be918097c8dcd518862
acaravantesnov <acaravantesnov@hotmail.com>
2021-12-26 11:45:12 +0100

    261220211

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: acaravan <acaravan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 05:10:36 by acaravan          #+#    #+#             */
-/*   Updated: 2021/12/21 15:42:39 by acaravan         ###   ########.fr       */
+/*   Updated: 2021/12/21 21:02:22 by acaravan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
 
 int		ft_atoi(char *str);
 int		ft_isdigit(int c);
--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: acaravan <acaravan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 04:40:45 by acaravan          #+#    #+#             */
-/*   Updated: 2021/12/21 15:43:41 by acaravan         ###   ########.fr       */
+/*   Updated: 2021/12/26 11:44:33 by acaravan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 // ARG="4 67 3 87 23"; ./push_swap $ARG | ./checker_OS $ARG
 
+/*	COSAS POR HACER
+
+	#	Numeric integer limits
+
+*/
+
 int	check_params(int argc)
 {
 	if (argc < 2)
-	{
-		write(1, "Error\n", 6);
 		return (-1);
-	}
 	return (0);
 }
 
@@ -44,6 +47,29 @@ int	ft_isnumber(char *num)
 	return (1);
 }
 
+void	check_repeated_numbers(int *stackA, int ndigitsA)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (i < ndigitsA)
+	{
+		j = 0;
+		while (j < ndigitsA)
+		{
+			if ((stackA[i] == stackA[j]) && (i != j))
+			{
+				write(1, "Error\n", 6);
+				exit(0);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 void	parsing(int *stackA, int ndigitsA, char **argv)
 {
 	int i;
@@ -51,7 +77,8 @@ void	parsing(int *stackA, int ndigitsA, char **argv)
 	i = 0;
 	while (i < ndigitsA)
 	{
-		if (ft_isnumber(argv[i + 1]) == 1)
+		if ((ft_isnumber(argv[i + 1]) == 1) && (ft_atoi(argv[i + 1]) > -2147483648) &&
+		(ft_atoi(argv[i + 1]) < 2147483647))
 			stackA[i] = ft_atoi(argv[i + 1]);
 		else
 		{
@@ -60,6 +87,7 @@ void	parsing(int *stackA, int ndigitsA, char **argv)
 		}
 		i++;
 	}
+	check_repeated_numbers(stackA, ndigitsA);
 }
 
 int check_sorted(int *stackA, int ndigitsA)
@@ -85,41 +113,18 @@ int check_sorted(int *stackA, int ndigitsA)
 	return (-1);
 }
 
-int	check_stackA(int *stackA, int ndigitsA)
-{
-	int i;
-
-	i = 0;
-	while (i < ndigitsA)
-	{
-		if ((stackA[i] > 2147483647) || (stackA[i] < -2147483648))
-		{
-			write(1, "Error\n", 6);
-			return (-1);
-		}
-		i++;
-	}
-	return (0);
-}
-
 int	main(int argc, char **argv)
 {
-	int i;
 	int *stackA;
 	int *stackB;
 	int ndigitsA;
-	int ndigitsB;
 
-	i = 0;
 	if (check_params(argc) == -1)
 		return (0);
 	ndigitsA = argc - 1;
-	ndigitsB = 0;
 	stackA = (int *)malloc(sizeof(int) * ndigitsA);
-	stackB = NULL;
+	stackB = (int *)malloc(sizeof(int) * ndigitsA);
 	parsing(stackA, ndigitsA, argv);
-	if (check_stackA(stackA, ndigitsA) == -1)
-		return (0);
 	if (check_sorted(stackA, ndigitsA) == 0)
 		return (0);
 	build_stacks(stackA, stackB, ndigitsA);
