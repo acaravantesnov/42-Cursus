9e1f0edc8e180c1d123253b728a0594e18f3b155
acaravan <acaravantesnov@hotmail.com>
2022-01-28 13:34:10 +0100

    280120221

--- Philosophers/philo/includes/philo.h
+++ Philosophers/philo/includes/philo.h
@@ -7,6 +7,31 @@
 # include <sys/time.h>
 # include <stdio.h>
 
+/*
+ * -- Philosophers --
+*/ 
+struct	Philosopher
+{
+	int	N;
+	int	activity;
+};
+
+/*
+ * -- Rules --
+*/
+struct	Rules
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
+	pthread_t	*ph;
+} rules;
+
+/*
+ * -- Utils --
+*/
 int	ft_atoi(char *str);
 
 #endif
--- Philosophers/philo/main.c
+++ Philosophers/philo/main.c
@@ -1,51 +1,52 @@
 #include "./includes/philo.h"
 
-void*	philosopher(void *index)
+int	freephandexit(pthread_t *ph)
 {
-	int N;
-	//int activity;
+	free(ph);
+	return (1);
+}
 
-	N = *(int*)index + 1;
-	printf("%d\n", N);
-	free(index);
-	return (NULL);
+void*	philosopher(void* arg)
+{
+	struct Philosopher ph;
+	ph.N = *(int*)arg + 1;
+	printf("%d", ph.N);
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
 	int i;
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	number_of_times_each_philosopher_must_eat = 0;
-	pthread_t	ph[ft_atoi(argv[1])];
 
 	if ((argc != 5) && (argc != 6))
 		return (1);
-	number_of_philosophers = ft_atoi(argv[1]);
-	time_to_die = ft_atoi(argv[2]);
-	time_to_eat = ft_atoi(argv[3]);
-	time_to_sleep = ft_atoi(argv[4]);
+
+	rules.ph = malloc(sizeof(pthread_t) * ft_atoi(argv[1]));
+	rules.number_of_philosophers = ft_atoi(argv[1]);
+	rules.time_to_die = ft_atoi(argv[2]);
+	rules.time_to_eat = ft_atoi(argv[3]);
+	rules.time_to_sleep = ft_atoi(argv[4]);
+	
 	if (argc == 6)
-		number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
-	printf("%d %d %d %d %d\n", number_of_philosophers, time_to_die, time_to_eat, time_to_sleep, number_of_times_each_philosopher_must_eat);
+		rules.number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
+	printf("%d %d %d %d %d\n", rules.number_of_philosophers, rules.time_to_die, rules.time_to_eat, rules.time_to_sleep, rules.number_of_times_each_philosopher_must_eat);
 	
 	i = 0;
-	while (i < number_of_philosophers)
+	while (i < rules.number_of_philosophers)
 	{
-		int* ptr = malloc (sizeof(int));
-		*ptr = i;
-		if (pthread_create(&ph[i], NULL, &philosopher, ptr) != 0)
-			return (1);
+		int* index;
+		index = malloc(sizeof(int));
+		*index = i;
+		if (pthread_create(&rules.ph[i], NULL, &philosopher, (void*)index) != 0)
+			return (freephandexit(rules.ph));
 		i++;
 	}
 
 	i = 0;
-	while (i < number_of_philosophers)
+	while (i < rules.number_of_philosophers)
 	{
-		if (pthread_join(ph[i], NULL) != 0)
-			return (1);
+		if (pthread_join(rules.ph[i], NULL) != 0)
+			return (freephandexit(rules.ph));
 		i++;
 	}
 	return (0);
