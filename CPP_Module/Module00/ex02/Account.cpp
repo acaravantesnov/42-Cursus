37048b983c4e5f49a00d58d71954fd2cd614b035
acaravantesnov <acaravantesnov@hotmail.com>
2022-02-22 17:44:46 +0100

    1000

--- CPP_Module/Module00/ex02/Account.cpp
+++ CPP_Module/Module00/ex02/Account.cpp
@@ -9,6 +9,7 @@ int	Account::_totalNbWithdrawals = 0;
 
 void	Account::displayAccountsInfos( void )
 {
+	_displayTimestamp();
 	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
@@ -18,16 +19,19 @@ Account::Account( int initial_deposit )
 	t::_accountIndex = _nbAccounts - 1;
 	t::_amount = initial_deposit;
 	_totalAmount += initial_deposit;
+	_displayTimestamp();
 	std::cout << "index:" << t::_accountIndex << ";amount:" << t::_amount << ";created" << std::endl;
 }
 
 Account::~Account( void )
 {
+	_displayTimestamp();
 	std::cout << "index:" << t::_accountIndex << ";amount:" << t::_amount << ";closed" << std::endl;
 }
 
 void	Account::makeDeposit( int deposit )
 {
+	_displayTimestamp();
 	std::cout << "index:" << t::_accountIndex << ";p_amount:" << t::_amount;
 	t::_amount += deposit;
 	t::_nbDeposits++;
@@ -38,6 +42,7 @@ void	Account::makeDeposit( int deposit )
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
+	_displayTimestamp();
 	std::cout << "index:" << t::_accountIndex << ";p_amount:" << t::_amount;
 	if (t::_amount >= withdrawal)
 	{
@@ -57,6 +62,30 @@ bool	Account::makeWithdrawal( int withdrawal )
 
 void	Account::displayStatus( void ) const
 {
+	_displayTimestamp();
 	std::cout << "index:" << t::_accountIndex << ";amount:" << t::_amount << ";deposits:" << t::_nbDeposits << ";withdrawals:" << t::_nbWithdrawals << std::endl;
+}
+
+void	Account::_displayTimestamp( void )
+{
+	std::time_t now = std::time(0);
+	tm	*time = localtime(&now);
+	std::cout << '[' << 1900 + time->tm_year;
+	if (time->tm_mon <= 9)
+		std::cout << "0";
+	std::cout << time->tm_mon + 1;
+	if (time->tm_mday <= 9)
+		std::cout << "0";
+	std::cout << time->tm_mday;
+	std::cout << "_";
+	if (time->tm_hour <= 9)
+		std::cout << "0";
+	std::cout << time->tm_hour;
+	if (time->tm_min <= 9)
+		std::cout << "0";
+	std::cout << time->tm_min;
+	if (time->tm_sec <= 9)
+		std::cout << "0";
+	std::cout << time->tm_sec << "] ";
 }
 
