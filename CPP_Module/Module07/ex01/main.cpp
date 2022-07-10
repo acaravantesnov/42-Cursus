23bc96adc3a838a39abf6a164637cd06e312efb7
acaravantesnov@hotmail.com <acaravan@c1r10s6.42madrid.com>
2022-07-10 14:40:02 +0200

    07/02

--- CPP_Module/Module07/ex01/main.cpp
+++ CPP_Module/Module07/ex01/main.cpp
@@ -6,19 +6,28 @@
 /*   By: acaravan <acaravan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 01:57:22 by acaravan          #+#    #+#             */
-/*   Updated: 2022/07/10 03:42:56 by acaravan         ###   ########.fr       */
+/*   Updated: 2022/07/10 14:28:06 by acaravan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-#define N 3
+#define N 10
+
+void	myfunc(int n)
+{
+	n *= 2;
+	std::cout << n << std::endl;
+}
 
 int main(void)
 {
-	std::string	arr[N];
-	std::string *arrptr = arr;
-	void *ptr = &strlen;
-	iter<std::string>(arrptr, N, ptr);
+	int	arr[N];
+	for (int i = 1; i <= N; i++)
+		arr[i - 1] = i;
+	int	*arrptr = arr;
+	void	(*funcptr)(int) = &myfunc;
+	iter<int>(arrptr, N, funcptr);
+
 	return (0);
 }
--- CPP_Module/Module07/ex02/Array.hpp
+++ CPP_Module/Module07/ex02/Array.hpp
@@ -1,26 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acaravan <acaravan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/10 01:56:53 by acaravan          #+#    #+#             */
-/*   Updated: 2022/07/10 14:28:05 by acaravan         ###   ########.fr       */
+/*   Created: 2022/07/10 14:29:27 by acaravan          #+#    #+#             */
+/*   Updated: 2022/07/10 14:39:12 by acaravan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ITER_HPP
-# define ITER_HPP
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
 
-# include <iostream>
+#include <iostream>
 
 template<typename T>
-T	iter(T *arr, int len, void (*funcptr)(T))
+class	Array
 {
-	for (int i = 0; i < len; i++)
-		funcptr(arr[i]);
-	return (*arr);
-}
+	private:
+		T	*arr;
+	public:
+		Array() {arr = new[] T;}
+		Array(unsigned int n) {arr = new[](n) T;}
+		Array(const Array &array);
+		~Array();
+		Array	&operator=(std::ostream &stream, const Array & array);
+};
 
 #endif
