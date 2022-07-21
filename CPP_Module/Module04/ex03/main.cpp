58433ad0f51378ce44dd577184aac397ce119a90
acaravantesnov <acaravantesnov@hotmail.com>
2022-07-21 15:56:35 +0200

    NFC

--- CPP_Module/Module04/ex03/main.cpp
+++ CPP_Module/Module04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acaravan <acaravan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 14:50:53 by acaravan          #+#    #+#             */
-/*   Updated: 2022/07/21 14:32:24 by acaravan         ###   ########.fr       */
+/*   Updated: 2022/07/21 15:48:06 by acaravan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,8 @@
 #include "Cure.hpp"
 #include "MateriaSource.hpp"
 
-void	leaks()
-{
-	system("leaks Interface");
-}
-
 int main()
 {
-	atexit(leaks);
 	Ice			i;
 	Cure 		c;
 
@@ -33,8 +27,11 @@ int main()
 
 	Character	ch1("Alberto");
 	Character	ch2("Juan");
-	ch1.equip(ms.createMateria("cure"));
+	AMateria *ptr = ms.createMateria("cure");
+	ch1.equip(ptr);
+	ch1.equip(&i);
 	ch1.use(0, ch2);
 
+	delete (ptr);
 	return (0);
 }
