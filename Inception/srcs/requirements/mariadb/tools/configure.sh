6f50ad42ef7489d4bd028e42c29b2d5f02dcb93a
acaravantesnov <acaravantesnov@hotmail.com>
2022-12-24 14:56:28 +0100

    Small changes

--- Inception/srcs/requirements/mariadb/tools/configure.sh
+++ Inception/srcs/requirements/mariadb/tools/configure.sh
@@ -14,18 +14,29 @@ chmod 777 /var/lib/mysql
 chmod 777 /var/run/mysqld
 mysql_install_db --user=$MYSQL_USR --datadir=/var/lib/mysql > /dev/null
 
+sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/my.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/my.cnf
+
 # Start mysqld and grant privileges
 touch temp
 cat << EOF > temp
 USE mysql;
-USE wordpress;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'acaravan.123' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
+DELETE FROM	mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PSSWD';
+CREATE DATABASE $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$WP_DB_USR'@'%' IDENTIFIED by '$WP_DB_PSSWD';
+
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USR'@'%';
+
+FLUSH PRIVILEGES;
 EOF
 /usr/sbin/mysqld --user=$MYSQL_USR --bootstrap < temp
 rm -f temp
-
-sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/my.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/my.cnf
 
 # Run mysqld
 exec /usr/sbin/mysqld --user=$MYSQL_USR --console
--- Inception/srcs/requirements/wordpress/tools/configure.sh
+++ Inception/srcs/requirements/wordpress/tools/configure.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-# Wait for MySQL to be ready
-while ! mariadb -h$MYSQL_HOST -u$MYSQL_USR -p$WP_DB_PSSWD $WP_DB_NAME &>/dev/null; do
+while ! mariadb -h$MYSQL_HOST -u$WP_DATABASE_USR -p$WP_DATABASE_PWD $WP_DATABASE_NAME &>/dev/null; do
     sleep 3
 done
 
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 
-# Install Wordpress
-wget https://wordpress.org/latest.tar.gz
-tar -xvzf latest.tar.gz
-rm -rf latest.tar.gz
-
 # Configure Wordpress
 chown -R www-data:www-data /var/www/html/wordpress
 chmod -R 755 /var/www/html/wordpress
 
-wp core download --allow-root
-wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USR --dbpass=$WP_DB_PSSWD \
-				--allow-root
+# Configure Wordpress
+cat wp-config-sample.php | sed -e "s/database_name_here/$WP_DB_NAME/g" \
+	-e "s/username_here/$WP_DB_USR/g" \
+	-e "s/password_here/$WP_DB_PSSWD/g" \
+	-e "s/localhost/$MYSQL_HOST/g" > wp-config.php
+
 wp core install --url=acaravan.42.fr --title=acaravan_webpage \
 				--admin_user=acaravan --admin_password=acaravan.123 \
 				--admin_email=acaravan@student.42madrid.com --allow-root
 #wp user create notadmin --porcelain --allow-root
+
 
 /usr/sbin/php-fpm7.3 -F -R
 #tail -f /dev/null
